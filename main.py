4cdd9b507a60802bfadb06b4d0dfaa1ae86786ff
Zoe Shao <zoshao@g.hmc.edu>
2023-07-23 09:48:09 -0700

    record audio

---
+++ main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import os
+import numpy as np
+from io import BytesIO
+import streamlit.components.v1 as components
+from st_custom_components import st_audiorec
+
+def transcribe_audio(audio):
+    # code to transcribe audio to text using Speech-to-Text API
+    return text
+
+def generate_response(text):
+    # code to generate response from GPT model
+    return response
+
+def synthesize_speech(text):
+    # code to convert text to speech using Text-to-Speech API
+    return audio
+
+def main():
+    st.title("Voice Chatbot")
+    if st.button('Record'):
+        # Step 1: Record audio
+        wav_audio_data = st_audiorec()
+
+        if wav_audio_data is not None:
+            # display audio data as received on the backend
+            st.audio(wav_audio_data, format='audio/wav')
+            
+
+        # Step 2: Transcribe audio to text
+        text = transcribe_audio(audio)
+        
+        # Step 3: Generate response using GPT model
+        response = generate_response(text)
+
+        # Step 4: Synthesize response to speech
+        response_audio = synthesize_speech(response)
+
+        # Play the response audio
+        st.audio(response_audio)
+
+if __name__ == '__main__':
+    main()
